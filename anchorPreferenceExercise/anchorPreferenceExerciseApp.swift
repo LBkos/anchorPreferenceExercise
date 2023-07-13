b22021a79e2eddead1c26d601f9a1342143d821b
LBkos <constantin.l.o.p@gmail.com>
2023-07-13 10:41:06 +0700

    Initial Commit

---
+++ anchorPreferenceExercise/anchorPreferenceExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  anchorPreferenceExerciseApp.swift
+//  anchorPreferenceExercise
+//
+//  Created by Константин Лопаткин on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct anchorPreferenceExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
